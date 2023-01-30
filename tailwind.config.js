3e57f07a488a4aed2be789cdfdbc2183609bbc01
Tedy Khoirul Abidin <tedikhoirulabidin@gmail.com>
2023-01-30 16:03:31 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/main.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light", "night"],
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+}
